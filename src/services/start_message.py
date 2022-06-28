1d1fa69edb3c07a93e1b6d640be35aa2d3bb91d2
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2022-06-28 12:45:06 +0300

    fix test

--- src/services/start_message.py
+++ src/services/start_message.py
@@ -1,10 +1,24 @@
 import json
-from typing import Union
+from typing import Union, Optional
+
+from pydantic import BaseModel, ValidationError
 
 
-def get_start_message_query(message: str) -> Union[int, dict[str, str], None]:
-    splitted_message = message.split(' ')
-    if len(splitted_message) == 1:
-        return None
-    return json.loads(' '.join(splitted_message[1:]))
+class StartMessageMeta(BaseModel):
+    referrer: Optional[int] = None
 
+
+def get_start_message_query(message: str) -> StartMessageMeta:
+    try:
+        splitted_message = message.split(' ')
+        if len(splitted_message) == 1:
+            return StartMessageMeta(referrer=None)
+
+        # new format
+        message_raw_meta = ' '.join(splitted_message[1:])
+        try:
+            return StartMessageMeta.parse_raw(message_raw_meta)
+        except ValidationError:
+            return StartMessageMeta(referrer=int(splitted_message[1]))
+    except Exception as e:
+        return StartMessageMeta(referrer=None)
--- src/tests/test_start_message.py
+++ src/tests/test_start_message.py
@@ -1,19 +1,18 @@
-from services.start_message import get_start_message_query
+import pytest
+
+from services.start_message import get_start_message_query, StartMessageMeta
 
 
-def test_old_format():
-    got = get_start_message_query('/start 89238')
+@pytest.mark.parametrize('input_,expected', [
+    ('/start 89238', StartMessageMeta(referrer=89238)),
+    ('/start 834ruiou', StartMessageMeta(referrer=None)),
+    ('/start {"referrer": 28934}', StartMessageMeta(referrer=28934)),
+    ('/start', StartMessageMeta(referrer=None)),
+    ('/start {"some_param": "value"}', StartMessageMeta(referrer=None)),
+    ('/start {"referrer":28934}', StartMessageMeta(referrer=28934)),
+    ('/start {"referrer":"28934iwe"}', StartMessageMeta(referrer=None)),
+])
+def test(input_, expected):
+    got = get_start_message_query(input_)
 
-    assert got == 89238
-
-
-def test_new_format():
-    got = get_start_message_query('/start {"referrer": 28934}')
-
-    assert got == {'referrer': 28934}
-
-
-def test_empty_queryes():
-    got = get_start_message_query('/start')
-
-    assert got is None
+    assert got == expected
