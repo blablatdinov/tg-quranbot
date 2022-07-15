65abe49e0f69033662c15445d89385adf8dd74aa
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2022-07-15 14:35:47 +0300

    lint

---
+++ src/handlers/button_handlers/ayat_search_by_sura_ayat_num_pagination.py
@@ -0,0 +1,38 @@
+
+from aiogram import types
+
+from db import db_connection
+from repository.ayats.ayat import AyatRepository
+from repository.ayats.favorite_ayats import FavoriteAyatsRepository
+from repository.ayats.neighbor_ayats import NeighborAyatsRepository
+from services.ayats.ayat_by_id import AyatById
+from services.ayats.ayat_search import SearchAnswer
+from services.ayats.enums import AyatPaginatorCallbackDataTemplate
+from services.ayats.keyboard import AyatSearchKeyboard
+from services.ayats.search_by_sura_ayat_num import AyatSearchWithNeighbors
+from services.regular_expression import IntableRegularExpression
+
+
+async def ayat_from_callback_handler(callback_query: types.CallbackQuery):
+    """Получить аят из кнопки в клавиатуре под результатом поиска.
+
+    :param callback_query: app_types.CallbackQuery
+    """
+    async with db_connection() as connection:
+        ayat_search = AyatSearchWithNeighbors(
+            AyatById(
+                AyatRepository(connection),
+                IntableRegularExpression(r'\d+', callback_query.data),
+            ),
+            NeighborAyatsRepository(connection),
+        )
+        answer = await SearchAnswer(
+            ayat_search,
+            AyatSearchKeyboard(
+                ayat_search,
+                FavoriteAyatsRepository(connection),
+                callback_query.from_user.id,
+                AyatPaginatorCallbackDataTemplate.ayat_search_template,
+            ),
+        ).to_answer()
+        await answer.send(callback_query.from_user.id)
--- src/handlers/button_handlers/favorite_ayats.py
+++ src/handlers/button_handlers/favorite_ayats.py
@@ -1,49 +1,18 @@
 from aiogram import types
-from aiogram.dispatcher import FSMContext
 
 from db import db_connection
 from repository.ayats.ayat import AyatRepository
 from repository.ayats.favorite_ayats import FavoriteAyatsRepository
-from repository.ayats.neighbor_ayats import (
-    FavoriteAyatsNeighborRepository,
-    NeighborAyatsRepository,
-    TextSearchNeighborAyatsRepository,
-)
+from repository.ayats.neighbor_ayats import FavoriteAyatsNeighborRepository, NeighborAyatsRepository
 from services.ayats.ayat_by_id import AyatById
 from services.ayats.ayat_search import AyatFavoriteStatus, FavoriteAyats, SearchAnswer
 from services.ayats.enums import AyatPaginatorCallbackDataTemplate
 from services.ayats.keyboard import AyatSearchKeyboard
 from services.ayats.search_by_sura_ayat_num import AyatSearchWithNeighbors
-from services.ayats.search_by_text import AyatSearchByTextAndId, AyatSearchByTextWithNeighbors
 from services.regular_expression import IntableRegularExpression
 from utlls import get_bot_instance
 
 bot = get_bot_instance()
-
-
-async def ayat_from_callback_handler(callback_query: types.CallbackQuery):
-    """Получить аят из кнопки в клавиатуре под результатом поиска.
-
-    :param callback_query: app_types.CallbackQuery
-    """
-    async with db_connection() as connection:
-        ayat_search = AyatSearchWithNeighbors(
-            AyatById(
-                AyatRepository(connection),
-                IntableRegularExpression(r'\d+', callback_query.data),
-            ),
-            NeighborAyatsRepository(connection),
-        )
-        answer = await SearchAnswer(
-            ayat_search,
-            AyatSearchKeyboard(
-                ayat_search,
-                FavoriteAyatsRepository(connection),
-                callback_query.from_user.id,
-                AyatPaginatorCallbackDataTemplate.ayat_search_template,
-            ),
-        ).to_answer()
-        await answer.send(callback_query.from_user.id)
 
 
 async def add_to_favorite(callback_query: types.CallbackQuery):
@@ -133,36 +102,6 @@ async def favorite_ayat(callback_query: types.CallbackQuery):
                 FavoriteAyatsRepository(connection),
                 callback_query.from_user.id,
                 AyatPaginatorCallbackDataTemplate.favorite_ayat_template,
-            ),
-        ).to_answer()
-
-    await answer.send(callback_query.from_user.id)
-
-
-async def ayats_search_buttons(callback_query: types.CallbackQuery, state: FSMContext):
-    """Обработчик кнопок для пролистывания по резултатам поиска.
-
-    :param callback_query: app_types.CallbackQuery
-    :param state: FSMContext
-    """
-    async with db_connection() as connection:
-        state_data = await state.get_data()
-        search_query = state_data['search_query']
-        ayat_search = AyatSearchByTextWithNeighbors(
-            AyatSearchByTextAndId(
-                AyatRepository(connection),
-                search_query,
-                IntableRegularExpression(r'\d+', callback_query.data),
-            ),
-            TextSearchNeighborAyatsRepository(connection, search_query),
-        )
-        answer = await SearchAnswer(
-            ayat_search,
-            AyatSearchKeyboard(
-                ayat_search,
-                FavoriteAyatsRepository(connection),
-                callback_query.from_user.id,
-                AyatPaginatorCallbackDataTemplate.ayat_text_search_template,
             ),
         ).to_answer()
 
