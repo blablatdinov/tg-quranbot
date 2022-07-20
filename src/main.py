c9d098e0a6434d2606d65a3a680bcbec9fedce9e
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2022-07-20 14:45:00 +0300

    lint

--- src/main.py
+++ src/main.py
@@ -1,12 +1,11 @@
 import logging
 
-from aiogram import Dispatcher, executor, types
+from aiogram import Dispatcher, executor
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 from aiogram.utils.executor import start_webhook
 
 from handlers.register import register_handlers
 from settings import settings
-from aiogram.dispatcher.webhook import SendMessage
 from utlls import get_bot_instance
 
 bot = get_bot_instance()
@@ -18,20 +17,20 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',  # noqa: WPS323 logger formatting
 )
 
-WEBHOOK_HOST = 'https://quranbot.ilaletdinov.ru'
-WEBHOOK_PATH = '/bot'
-WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
-WEBAPP_HOST = 'localhost'
-WEBAPP_PORT = 8010
+
+async def on_startup(dispatcher: Dispatcher):
+    """Действия, выполняемые при старте приложения.
+
+    :param dispatcher: Dispatcher
+    """
+    await bot.set_webhook(settings.webhook_url)
 
 
-async def on_startup(dp):
-    logging.warning('Setting webhook...')
-    res = await bot.set_webhook(WEBHOOK_URL)
-    logging.warning('Setting webhook res: %s', res)
+async def on_shutdown(dispatcher: Dispatcher):
+    """Действия, выполняемые при остановке приложения.
 
-
-async def on_shutdown(dp):
+    :param dispatcher: Dispatcher
+    """
     logging.warning('Shutting down..')
     await bot.delete_webhook()
     await dp.storage.close()
@@ -46,10 +45,10 @@ if __name__ == '__main__':
     else:
         start_webhook(
             dispatcher=dp,
-            webhook_path=WEBHOOK_PATH,
+            webhook_path=settings.WEBHOOK_PATH,
             on_startup=on_startup,
             on_shutdown=on_shutdown,
             skip_updates=True,
-            host=WEBAPP_HOST,
-            port=WEBAPP_PORT,
+            host=settings.WEBAPP_HOST,
+            port=settings.WEBAPP_PORT,
         )
