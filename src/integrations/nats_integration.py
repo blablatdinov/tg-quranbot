9835fcbcf25e9a9cdb985e4567d03461add4e059
blablatdinov <45946541+blablatdinov@users.noreply.github.com>
2022-08-08 18:40:08 +0300

    change queue name (#80)

--- src/integrations/nats_integration.py
+++ src/integrations/nats_integration.py
@@ -26,7 +26,7 @@ class NatsIntegration(object):
         """Прием сообщений."""
         nats_client = await nats.connect('localhost')
         logger.info('Start handling events...')
-        await nats_client.subscribe('foo', cb=self._message_handler)
+        await nats_client.subscribe('default', cb=self._message_handler)
         while True:  # noqa: WPS457
             await asyncio.sleep(1)
 
