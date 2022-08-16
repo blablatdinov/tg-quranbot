3f5951488f01bd1609ad65cdfb4d345fac5110f4
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2022-08-16 21:02:54 +0300

    test CityRepository

--- src/repository/city.py
+++ src/repository/city.py
@@ -1,13 +1,14 @@
 import abc
+import uuid
 
-from asyncpg import Connection
+from databases import Database
 from pydantic import BaseModel, parse_obj_as
 
 
 class City(BaseModel):
     """Модель города."""
 
-    id: int
+    id: uuid.UUID
     name: str
 
 
@@ -27,8 +28,8 @@ class CityRepositoryInterface(object):
 class CityRepository(CityRepositoryInterface):
     """Класс для работы с городами в БД."""
 
-    def __init__(self, connection: Connection):
-        self.connection = connection
+    def __init__(self, connection: Database):
+        self._connection = connection
 
     async def search_by_name(self, search_query: str) -> list[City]:
         """Поиск по имени.
@@ -37,6 +38,6 @@ class CityRepository(CityRepositoryInterface):
         :returns: list[City]
         """
         search_query = '%{0}%'.format(search_query)
-        query = 'SELECT id, name FROM prayer_city WHERE name ILIKE $1'
-        rows = await self.connection.fetch(query, search_query)
-        return parse_obj_as(list[City], rows)
+        query = 'SELECT city_id AS id, name FROM cities WHERE name ILIKE :search_query'
+        rows = await self._connection.fetch_all(query, {'search_query': search_query})
+        return parse_obj_as(list[City], [row._mapping for row in rows])
