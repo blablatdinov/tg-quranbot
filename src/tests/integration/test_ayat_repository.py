d53a302f5986f3c2ee7c91bde27581b9e61c713f
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2022-08-23 17:28:26 +0300

    create factory

--- src/tests/integration/test_ayat_repository.py
+++ src/tests/integration/test_ayat_repository.py
@@ -1,23 +1,17 @@
 import pytest
 
 from repository.ayats.ayat import Ayat, AyatRepository
+from tests.factories.file_factory import factory
 
 
 @pytest.fixture()
 async def ayat(db_session, mixer):
     await db_session.execute("INSERT INTO suras (sura_id, link) VALUES (1, '/hello')")
-    insert_file_query = """
-        INSERT INTO files
-        (file_id, telegram_file_id, created_at, link)
-        VALUES
-        ('8f6e2fa5-1a26-4e7a-bd58-7597385121fa', 'file_id', '2030-01-03', 'file/link')
-    """
-    await db_session.execute(insert_file_query)
     ayats_insert_query = """
         INSERT INTO ayats
-        (ayat_id, public_id, sura_id, day, ayat_number, content, arab_text, transliteration, audio_id)
+        (ayat_id, public_id, sura_id, ayat_number, content, arab_text, transliteration, audio_id)
         VALUES
-        (:ayat_id, :public_id, :sura_id, :day, :ayat_number, :content, :arab_text, :transliteration, :audio_id)
+        (:ayat_id, :public_id, :sura_id, :ayat_number, :content, :arab_text, :transliteration, :audio_id)
         """
     await db_session.execute_many(
         ayats_insert_query,
@@ -26,23 +20,21 @@ async def ayat(db_session, mixer):
                 'ayat_id': 1,
                 'public_id': '96710161-75ab-483f-a9bb-e4d86e068936',
                 'sura_id': 1,
-                'day': 1,
                 'ayat_number': '1',
                 'content': 'content',
                 'arab_text': 'arab text',
                 'transliteration': 'transliteration',
-                'audio_id': '8f6e2fa5-1a26-4e7a-bd58-7597385121fa',
+                'audio_id': str(await factory(db_session)),
             },
             {
                 'ayat_id': 2,
                 'public_id': '96710161-75ab-483f-a9bb-e4d86e068936',
                 'sura_id': 1,
-                'day': 2,
                 'ayat_number': '2',
                 'content': 'content',
                 'arab_text': 'arab text',
                 'transliteration': 'transliteration',
-                'audio_id': '8f6e2fa5-1a26-4e7a-bd58-7597385121fa',
+                'audio_id': str(await factory(db_session)),
             },
         ],
     )
