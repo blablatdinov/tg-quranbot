dcb4f2c2de055f4d3925f5cf65924b602fbd46eb
Ilaletdinov Almaz <45946541+blablatdinov@users.noreply.github.com>
2022-10-01 23:11:20 +0300

    Ayat search error fix [skip ci] (#138)

---
+++ src/tests/test_cached_ayat_query_answer.py
@@ -0,0 +1,54 @@
+from contextlib import suppress
+from unittest.mock import AsyncMock
+
+import pytest
+from aioredis import Redis
+
+from integrations.tg.tg_answers import TgAnswerInterface
+from integrations.tg.tg_answers.update import Update
+from services.ayats.search_by_text import CachedAyatSearchQueryAnswer
+
+
+class FakeError(Exception):
+    pass
+
+
+class TgAnswerFake(TgAnswerInterface):
+
+    async def build(self, update):
+        raise FakeError
+
+
+@pytest.fixture()
+def mock_redis():
+    redis = Redis()
+    redis.set = AsyncMock()
+    return redis
+
+
+@pytest.fixture()
+def update():
+    update_json = """
+    {
+        "callback_query": null,
+        "inline_query": null,
+        "message": {
+            "chat": {
+                "id": 358610865
+            },
+            "location_": null,
+            "message_id": 22199,
+            "text": "камни"
+        },
+        "update_id": 637462858
+    }
+    """
+    return Update.parse_raw(update_json)
+
+
+async def test(update, mock_redis):
+    mock_redis.set: AsyncMock
+    with suppress(FakeError):
+        await CachedAyatSearchQueryAnswer(TgAnswerFake(), mock_redis).build(update)
+
+    mock_redis.set.assert_called_with('358610865:ayat_search_query', 'камни')
