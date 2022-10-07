ff014930b6de5f3541b23d728204570f17dc49be
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2022-10-07 23:57:37 +0300

    lint

--- src/services/reset_state_answer.py
+++ src/services/reset_state_answer.py
@@ -7,12 +7,18 @@ from services.user_state import UserState, UserStep
 
 
 class ResetStateAnswer(TgAnswerInterface):
+    """Декоратор для обнуления состояния пользователя."""
 
     def __init__(self, answer: TgAnswerInterface, redis: Redis):
         self._origin = answer
         self._redis = redis
 
     async def build(self, update: Update) -> list[httpx.Request]:
+        """Сборка ответа.
+
+        :param update: Update
+        :return: list[httpx.Request]
+        """
         requests = await self._origin.build(update)
         await UserState(
             self._redis, update.chat_id(),
