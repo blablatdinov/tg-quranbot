d37f5ca109827753c793330e0b1eef243fd11d3a
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2022-10-30 18:48:33 +0300

    lint

--- src/event_recieve.py
+++ src/event_recieve.py
@@ -42,6 +42,17 @@ class RecievedEventInterface(Protocol):
 class SendPrayersEvent(RecievedEventInterface):
     """Событие о рассылки времени намаза."""
 
+    _time_format = '%H:%M'
+    _template = '\n'.join([
+        'Время намаза для г. {city_name} ({date})\n',
+        'Иртәнге: {fajr_prayer_time}',
+        'Восход: {sunrise_prayer_time}',
+        'Өйлә: {dhuhr_prayer_time}',
+        'Икенде: {asr_prayer_time}',
+        'Ахшам: {magrib_prayer_time}',
+        'Ястү: {ishaa_prayer_time}',
+    ])
+
     def __init__(self, users_repo: UsersRepositoryInterface, empty_answer: TgAnswerInterface, database: Database):
         self._users_repo = users_repo
         self._empty_answer = empty_answer
@@ -68,18 +79,16 @@ class SendPrayersEvent(RecievedEventInterface):
         """
         deactivated_users = []
         chat_ids = await self._users_repo.active_users_with_city()
+        answers = await self._build_requests(chat_ids)
+        for response_list in await BulkSendableAnswer(answers).send(Update(update_id=0).json()):
+            for response_dict in response_list:
+                if not response_dict['ok']:
+                    deactivated_users.append(response_dict['chat_id'])
+        await self._users_repo.update_status(list(set(deactivated_users)), to=False)
+
+    async def _build_requests(self, chat_ids: list[int]) -> list[TgAnswerInterface]:
         answers: list[TgAnswerInterface] = []
         target_date = datetime.datetime.now() + datetime.timedelta(days=1)
-        time_format = '%H:%M'
-        template = '\n'.join([
-            'Время намаза для г. {city_name} ({date})\n',
-            'Иртәнге: {fajr_prayer_time}',
-            'Восход: {sunrise_prayer_time}',
-            'Өйлә: {dhuhr_prayer_time}',
-            'Икенде: {asr_prayer_time}',
-            'Ахшам: {magrib_prayer_time}',
-            'Ястү: {ishaa_prayer_time}',
-        ])
         for chat_id in chat_ids:
             safe_not_found_prayers = SafeNotFoundPrayers(
                 self._database,
@@ -99,15 +108,15 @@ class SendPrayersEvent(RecievedEventInterface):
                             TgMessageAnswer(self._empty_answer),
                             chat_id,
                         ),
-                        template.format(
+                        self._template.format(
                             city_name=prayers[0].city,
                             date=prayers[0].day.strftime('%d.%m.%Y'),
-                            fajr_prayer_time=prayers[0].time.strftime(time_format),
-                            sunrise_prayer_time=prayers[1].time.strftime(time_format),
-                            dhuhr_prayer_time=prayers[2].time.strftime(time_format),
-                            asr_prayer_time=prayers[3].time.strftime(time_format),
-                            magrib_prayer_time=prayers[4].time.strftime(time_format),
-                            ishaa_prayer_time=prayers[5].time.strftime(time_format),
+                            fajr_prayer_time=prayers[0].time.strftime(self._time_format),
+                            sunrise_prayer_time=prayers[1].time.strftime(self._time_format),
+                            dhuhr_prayer_time=prayers[2].time.strftime(self._time_format),
+                            asr_prayer_time=prayers[3].time.strftime(self._time_format),
+                            magrib_prayer_time=prayers[4].time.strftime(self._time_format),
+                            ishaa_prayer_time=prayers[5].time.strftime(self._time_format),
                         ),
                     ),
                     UserPrayersKeyboardByChatId(
@@ -117,11 +126,7 @@ class SendPrayersEvent(RecievedEventInterface):
                     ),
                 ),
             )
-        for response_list in await BulkSendableAnswer(answers).send(Update(update_id=0).json()):
-            for response_dict in response_list:
-                if not response_dict['ok']:
-                    deactivated_users.append(response_dict['chat_id'])
-        await self._users_repo.update_status(list(set(deactivated_users)), to=False)
+        return answers
 
 
 class RecievedEvents(Runable):
