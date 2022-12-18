cd417f1f5d9593c7a80d79d8db86e5701d96da18
Ilaletdinov Almaz <45946541+blablatdinov@users.noreply.github.com>
2022-12-18 17:41:15 +0300

    Fix deprecation warning [skip ci] (#200)

--- src/tests/test_user_state.py
+++ src/tests/test_user_state.py
@@ -1,4 +1,4 @@
-import asyncio
+from unittest.mock import AsyncMock
 
 import pytest
 from aioredis import Redis
@@ -8,13 +8,14 @@ from services.user_state import UserState, UserStep
 
 @pytest.fixture()
 def mock_redis(mocker):
-    future: asyncio.Future = asyncio.Future()
-    future.set_result(None)
-    mocker.patch('aioredis.Redis.get', return_value=future)
+    mock = AsyncMock(return_value=None)
+    Redis.get = mock  # type: ignore
+    return mock
 
 
 @pytest.mark.usefixtures('mock_redis')
-async def test_not_exists_state():
+async def test_not_exists_state(mock_redis):
     got = await UserState(Redis(), 879435).step()
 
     assert got == UserStep.nothing
+    mock_redis.assert_called_with('879435:step')
