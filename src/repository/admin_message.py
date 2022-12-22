66358cb820a21becf210977b668a96adda49f120
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2022-12-22 12:33:39 +0300

    Create handler for `/help` command

--- src/repository/admin_message.py
+++ src/repository/admin_message.py
@@ -22,6 +22,39 @@ class AdminMessageRepositoryInterface(Protocol):
         """
 
 
+class AdminMessageInterface(Protocol):
+    """Интерфейс административного сообщения."""
+
+    async def text(self) -> str:
+        """Чтение содержимого административного сообщения."""
+
+
+class AdminMessage(AdminMessageInterface):
+    """Административное сообщение."""
+
+    def __init__(self, key: str, connection: Database):
+        """Конструктор класса.
+
+        :param key: str
+        :param connection: Database
+        """
+        self._key = key
+        self._connection = connection
+
+    async def text(self) -> str:
+        """Текст административного сообщения.
+
+        :raises InternalBotError: возбуждается если административное сообщение с переданным ключом не найдено
+        :return: str
+        """
+        record = await self._connection.fetch_one(
+            'SELECT text FROM admin_messages m WHERE m.key = :key', {'key': self._key},
+        )
+        if not record:
+            raise InternalBotError('Не найдено административное сообщение с ключом {0}'.format(self._key))
+        return record._mapping['text']  # noqa: WPS437
+
+
 class AdminMessageRepository(AdminMessageRepositoryInterface):
     """Класс для работы с БД."""
 
