fa46a63ae3c6f7289d93f7965f21f92b87333d64
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2023-08-27 01:27:44 +0300

    User id bigint migrations

--- migrations/20230826_01_RChAw-change-chat-id-type.sql
+++ migrations/20230826_01_RChAw-change-chat-id-type.sql
@@ -1,4 +1,6 @@
 -- Change chat_id type
 -- depends: 20230815_01_7ALQG
-ALTER TABLE users ALTER COLUMN chat_id TYPE bigint USING chat_id::bigint;
+
+ALTER TABLE favorite_ayats ALTER COLUMN user_id TYPE bigint USING user_id::bigint;
+
 ALTER TABLE prayers_at_user ALTER COLUMN user_id TYPE bigint USING user_id::bigint;
