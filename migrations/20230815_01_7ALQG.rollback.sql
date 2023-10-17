96ca0845b9e2e90420fd33c11064bed96a20c64e
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2023-10-17 17:03:24 +0300

    Check migrations in CI

---
+++ migrations/20230815_01_7ALQG.rollback.sql
@@ -0,0 +1,26 @@
+-- Init migration.
+-- depends: 
+
+DROP TABLE favorite_ayats CASCADE;
+
+DROP TABLE files CASCADE;
+
+DROP TABLE cities CASCADE;
+
+DROP TABLE prayers_at_user_groups CASCADE;
+
+DROP TABLE prayers_at_user CASCADE;
+
+DROP TABLE prayer_days CASCADE;
+
+DROP TABLE suras CASCADE;
+
+DROP TABLE admin_messages CASCADE;
+
+DROP TABLE ayats CASCADE;
+
+DROP TABLE podcasts CASCADE;
+
+DROP TABLE prayers CASCADE;
+
+DROP TABLE users CASCADE;
