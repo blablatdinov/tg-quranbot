61a8fadcccdb19a816f50c51a2d86b2cf14214bf
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2024-01-22 16:10:25 +0300

    directory case

--- tests/test_define_files.py
+++ tests/test_define_files.py
@@ -18,6 +18,10 @@ def repo_path():
         open(temp_dir_path / filename, 'wb').close()
         repo.index.add([filename])
         repo.index.commit(filename)
+    os.mkdir(str(Path(temp_dir_path / 'dir1')))
+    Path(temp_dir_path / 'dir1/file_in_dir').write_text('File in directory')
+    repo.index.add(['dir1/file_in_dir'])
+    repo.index.commit('Create directory')
     Path(temp_dir_path / 'first').write_text('Some changes')
     repo.index.add(['first'])
     repo.index.commit('Some changes')
@@ -29,11 +33,11 @@ def repo_path():
 def test(repo_path):
     repo = Repo(repo_path)
     file_change_count = defaultdict(int)
-    exists_files = glob.glob('*', root_dir=repo_path, recursive=True)
+    exists_files = glob.glob('**/*', root_dir=repo_path, recursive=True)
     for commit in repo.iter_commits():
         for item in commit.stats.files.items():
             filename, stats = item
             if filename in exists_files:
                 file_change_count[filename] += stats['lines']
     sorted_files = sorted(file_change_count.items(), key=itemgetter(1))
-    assert [x[0] for x in sorted_files] == ['third', 'first']
+    assert [x[0] for x in sorted_files] == ['third', 'first', 'dir1/file_in_dir']
