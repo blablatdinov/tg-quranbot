b9bdec0f39da44cad42b81d9c9867cdb64c4946a
Ilaletdinov Almaz <45946541+blablatdinov@users.noreply.github.com>
2024-03-09 09:14:07 +0300

    Podcast showed status on undo reactions (#723)

--- src/tests/e2e/test_podcast.py
+++ src/tests/e2e/test_podcast.py
@@ -126,7 +126,7 @@ def test_reverse_reaction(tg_client, bot_name, wait_until, first_reaction, secon
 @pytest.mark.usefixtures('_bot_process', '_clear_db', '_user')
 @pytest.mark.parametrize('reaction', ['👎', '👍'])
 @pytest.mark.tg_button()
-def test_undo_reaction(tg_client, bot_name, wait_until, reaction):
+def test_undo_reaction(tg_client, bot_name, wait_until, reaction, db_query_vals):
     tg_client.send_message(bot_name, '🎧 Подкасты')
     messages = wait_until(tg_client, 6)
     next(
@@ -149,6 +149,7 @@ def test_undo_reaction(tg_client, bot_name, wait_until, reaction):
         for button_row in messages[0].get_buttons()
         for button in button_row
     ] == ['👍 0', '👎 0']
+    assert db_query_vals('SELECT reaction FROM podcast_reactions') == [('showed',)]
 
 
 @pytest.mark.usefixtures('_bot_process', '_clear_db')
