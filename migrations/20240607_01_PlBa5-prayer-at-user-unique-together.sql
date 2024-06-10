03092c3de528c0bada1a264c71724a7f1d8f2d80
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2024-06-10 16:23:07 +0300

    SQL query for remote prayer_at_user duplicates

--- migrations/20240607_01_PlBa5-prayer-at-user-unique-together.sql
+++ migrations/20240607_01_PlBa5-prayer-at-user-unique-together.sql
@@ -22,5 +22,18 @@
 -- Prayer at user unique together
 -- depends: 20240318_01_D8Zfn-using-timezones
 
+DELETE FROM prayers_at_user
+WHERE prayer_at_user_id IN (
+    SELECT unnest(subquery.ids_for_remove) FROM
+        (
+            SELECT
+                (array_agg(prayer_at_user_id))[2:] AS ids_for_remove,
+                count(*) AS records_count
+            FROM prayers_at_user
+            GROUP BY user_id, prayer_id
+            HAVING count(*) > 1
+        ) AS subquery
+);
+
 ALTER TABLE prayers_at_user
 ADD UNIQUE (user_id, prayer_id);
