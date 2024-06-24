532aa3c671457a24ee3fa1dad6233b15cc0c1910
Ilaletdinov Almaz <45946541+blablatdinov@users.noreply.github.com>
2024-06-24 23:33:32 +0300

    Down code duplication (#1071)

--- src/tests/it/srv/prayers/test_city.py
+++ src/tests/it/srv/prayers/test_city.py
@@ -20,9 +20,7 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 # OR OTHER DEALINGS IN THE SOFTWARE.
 
-import httpx
 import pytest
-import ujson
 
 from app_types.stringable import FkAsyncStr
 from exceptions.content_exceptions import CityNotSupportedError
@@ -31,58 +29,15 @@ from srv.prayers.city import CityIdByName, PgCity
 
 
 @pytest.fixture()
-def _mock_nominatim(respx_mock):
-    respx_mock.get(
-        'https://nominatim.openstreetmap.org/reverse.php?lat=55.7887&lon=49.1221&format=jsonv2',
-    ).mock(return_value=httpx.Response(
-        200,
-        text=ujson.dumps({
-            'address': {
-                'ISO3166-2-lvl4': 'RU-TA',
-                'city': 'Kazan',
-                'city_district': 'Вахитовский район',
-                'country': 'Россия',
-                'country_code': 'ru',
-                'county': 'городской округ Казань',
-                'house_number': '12',
-                'postcode': '420111',
-                'region': 'Приволжский федеральный округ',
-                'road': 'Университетская улица',
-                'state': 'Татарстан',
-                'suburb': 'Старо-Татарская слобода',
-            },
-            'addresstype': 'building',
-            'boundingbox': ['55.7886681', '55.7889864', '49.1218618', '49.1223806'],
-            'category': 'building',
-            'display_name': ' '.join([
-                '12, Университетская улица, Старо-Татарская слобода,',
-                'Вахитовский район, Казань, городской округ Казань,',
-                'Татарстан, Приволжский федеральный округ, 420111, Россия',
-            ]),
-            'importance': 9.9,
-            'lat': '55.7888272',
-            'licence': 'Data © OpenStreetMap contributors, ODbL 1.0. http://osm.org/copyright',
-            'lon': '49.12212116815689',
-            'name': '',
-            'osm_id': 105336438,
-            'osm_type': 'way',
-            'place_id': 128743364,
-            'place_rank': 30,
-            'type': 'apartments',
-        }),
-    ))
-
-
-@pytest.fixture()
 async def _db_city(pgsql):
-    await pgsql.execute("INSERT INTO cities (city_id, name) VALUES (:city_id, 'Kazan')", {
+    await pgsql.execute("INSERT INTO cities (city_id, name) VALUES (:city_id, 'Казань')", {
         'city_id': 'e9fa0fff-4e6a-47c8-8654-09adf913734a',
     })
 
 
 @pytest.mark.usefixtures('_db_city')
 async def test_city_id_by_name(pgsql):
-    got = await CityIdByName(FkAsyncStr('Kazan'), pgsql).to_str()
+    got = await CityIdByName(FkAsyncStr('Казань'), pgsql).to_str()
 
     assert got == 'e9fa0fff-4e6a-47c8-8654-09adf913734a'
 
@@ -98,12 +53,12 @@ async def test_pg_city(pgsql):
     city = PgCity(FkAsyncStr('e9fa0fff-4e6a-47c8-8654-09adf913734a'), pgsql)
 
     assert str(await city.city_id()) == 'e9fa0fff-4e6a-47c8-8654-09adf913734a'
-    assert await city.name() == 'Kazan'
+    assert await city.name() == 'Казань'
 
 
 @pytest.mark.usefixtures('_db_city', '_mock_nominatim')
 async def test_ctors(pgsql):
-    city_by_name = PgCity.name_ctor('Kazan', pgsql)
+    city_by_name = PgCity.name_ctor('Казань', pgsql)
     city_by_location = PgCity.location_ctor(FkCoordinates(55.7887, 49.1221), pgsql)
 
     assert str(await city_by_name.city_id()) == str(await city_by_location.city_id())
--- src/tests/unit/integrations/test_nominatim_city_name.py
+++ src/tests/unit/integrations/test_nominatim_city_name.py
@@ -20,72 +20,14 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 # OR OTHER DEALINGS IN THE SOFTWARE.
 
-from typing import override
-
-import httpx
 import pytest
-import ujson
 
 from integrations.nominatim import NominatimCityName
-from integrations.tg.coordinates import Coordinates
-
-
-class FkCoordinates(Coordinates):
-
-    @override
-    def latitude(self) -> float:
-        return 55.7887
-
-    @override
-    def longitude(self) -> float:
-        return 49.1221
-
-
-@pytest.fixture()
-def _mock_nominatim(respx_mock):
-    respx_mock.get(
-        'https://nominatim.openstreetmap.org/reverse.php?lat=55.7887&lon=49.1221&format=jsonv2',
-    ).mock(return_value=httpx.Response(
-        200,
-        text=ujson.dumps({
-            'address': {
-                'ISO3166-2-lvl4': 'RU-TA',
-                'city': 'Казань',
-                'city_district': 'Вахитовский район',
-                'country': 'Россия',
-                'country_code': 'ru',
-                'county': 'городской округ Казань',
-                'house_number': '12',
-                'postcode': '420111',
-                'region': 'Приволжский федеральный округ',
-                'road': 'Университетская улица',
-                'state': 'Татарстан',
-                'suburb': 'Старо-Татарская слобода',
-            },
-            'addresstype': 'building',
-            'boundingbox': ['55.7886681', '55.7889864', '49.1218618', '49.1223806'],
-            'category': 'building',
-            'display_name': ' '.join([
-                '12, Университетская улица, Старо-Татарская слобода,',
-                'Вахитовский район, Казань, городской округ Казань,',
-                'Татарстан, Приволжский федеральный округ, 420111, Россия',
-            ]),
-            'importance': 9.9,
-            'lat': '55.7888272',
-            'licence': 'Data © OpenStreetMap contributors, ODbL 1.0. http://osm.org/copyright',
-            'lon': '49.12212116815689',
-            'name': '',
-            'osm_id': 105336438,
-            'osm_type': 'way',
-            'place_id': 128743364,
-            'place_rank': 30,
-            'type': 'apartments',
-        }),
-    ))
+from integrations.tg.coordinates import FkCoordinates
 
 
 @pytest.mark.usefixtures('_mock_nominatim')
 async def test():
-    got = await NominatimCityName(FkCoordinates()).to_str()
+    got = await NominatimCityName(FkCoordinates(55.7887, 49.1221)).to_str()
 
     assert got == 'Казань'
