ff8611aa1b2e55853368ee65fcf1214a847882b8
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2024-09-02 23:58:12 +0300

    Lint

--- src/tests/e2e/test_prayer_times.py
+++ src/tests/e2e/test_prayer_times.py
@@ -20,6 +20,8 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 # OR OTHER DEALINGS IN THE SOFTWARE.
 
+# flake8: noqa: WPS202
+
 import datetime
 from itertools import chain
 
