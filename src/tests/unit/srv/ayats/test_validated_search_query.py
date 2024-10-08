926e8d68beccec7a3b7369aa85071f3d5dd2eebe
Ilaletdinov Almaz <45946541+blablatdinov@users.noreply.github.com>
2024-10-08 17:56:33 +0300

    Fix test (#1298)

--- src/tests/unit/srv/ayats/test_validated_search_query.py
+++ src/tests/unit/srv/ayats/test_validated_search_query.py
@@ -38,6 +38,10 @@ def _sura_num_invalid(num):
 
 
 def _ayat_num_invalid(ayat_num):
+    try:
+        int(ayat_num)
+    except ValueError:
+        return True
     return ayat_num.isdigit() and int(ayat_num) < 1
 
 
