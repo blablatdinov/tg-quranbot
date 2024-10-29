ccc5ed356fe077f29d2f61c43dfa119e5fc2406a
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2024-10-29 14:22:18 +0300

    Test for CityNames

---
+++ src/tests/it/srv/prayers/test_city_names.py
@@ -0,0 +1,42 @@
+# The MIT License (MIT).
+#
+# Copyright (c) 2018-2024 Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+# OR OTHER DEALINGS IN THE SOFTWARE.
+
+import pytest
+
+from app_types.fk_async_str import FkAsyncStr
+from exceptions.content_exceptions import CityNotSupportedError
+from integrations.tg.fk_coordinates import FkCoordinates
+from srv.prayers.city_id_by_name import CityIdByName
+from srv.prayers.pg_city import PgCity
+from srv.prayers.city_names import CityNames
+
+
+@pytest.fixture
+async def _db_city(city_factory):
+    await city_factory('e9fa0fff-4e6a-47c8-8654-09adf913734a', 'Казань')
+
+
+@pytest.mark.usefixtures('_db_city')
+async def test(pgsql):
+    names = await CityNames(pgsql, 'Ка').to_list()
+
+    assert names == ['Казань']
