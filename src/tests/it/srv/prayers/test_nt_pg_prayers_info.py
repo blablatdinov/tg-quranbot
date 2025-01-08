7ca66cb2e689881feabe9b2941daf48517ea98c4
Ilaletdinov Almaz <45946541+blablatdinov@users.noreply.github.com>
2025-01-08 21:33:08 +0300

    Separate user prayer creating (#1481)

--- src/tests/it/srv/prayers/test_nt_pg_prayers_info.py
+++ src/tests/it/srv/prayers/test_nt_pg_prayers_info.py
@@ -20,15 +20,15 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 # OR OTHER DEALINGS IN THE SOFTWARE.
 
+import datetime
 import uuid
 
 import pytest
 
-from integrations.tg.fk_chat_id import FkChatId
+from exceptions.prayer_exceptions import PrayersAlreadyExistsError
 from srv.prayers.fk_city import FkCity
 from srv.prayers.fk_prayers_info import FkPrayersInfo
 from srv.prayers.nt_pg_prayers_info import NtPgPrayersInfo
-from srv.prayers.nt_user_prayers_info import NtUserPrayersInfo
 
 
 @pytest.fixture
@@ -41,8 +41,8 @@ async def _user(user_factory):
     await user_factory(1, city=FkCity(uuid.UUID('e9fa0fff-4e6a-47c8-8654-09adf913734a'), 'Казань'))
 
 
-@pytest.fixture
-async def _prayers(pgsql):
+@pytest.mark.usefixtures('_db_city', '_user')
+async def test(pgsql):
     await NtPgPrayersInfo(
         FkPrayersInfo({
             'city_name': 'Казань',
@@ -57,39 +57,49 @@ async def _prayers(pgsql):
         pgsql,
     ).to_dict()
 
-
-@pytest.mark.usefixtures('_db_city', '_user', '_prayers')
-async def test(pgsql):
-    await NtUserPrayersInfo(
-        FkPrayersInfo({
-            'city_name': 'Казань',
-            'date': '21.10.2023',
-            'fajr_prayer_time': '04:22',
-            'sunrise_prayer_time': '06:26',
-            'dhuhr_prayer_time': '12:00',
-            'asr_prayer_time': '14:35',
-            'magrib_prayer_time': '16:30',
-            'ishaa_prayer_time': '18:12',
-        }),
-        pgsql,
-        FkChatId(1),
-    ).to_dict()
-
-    assert [
-        dict(row)
-        for row in await pgsql.fetch_all('select user_id, is_read from prayers_at_user')
-    ] == [
+    assert [dict(row) for row in await pgsql.fetch_all('select city_id, day, name, time from prayers')] == [
         {
-            'is_read': False,
-            'user_id': 1,
-        }
-        for _ in range(5)
+            'city_id': 'e9fa0fff-4e6a-47c8-8654-09adf913734a',
+            'day': datetime.date(2023, 10, 21),
+            'name': 'fajr',
+            'time': datetime.time(4, 22),
+        },
+        {
+            'city_id': 'e9fa0fff-4e6a-47c8-8654-09adf913734a',
+            'day': datetime.date(2023, 10, 21),
+            'name': 'sunrise',
+            'time': datetime.time(6, 26),
+        },
+        {
+            'city_id': 'e9fa0fff-4e6a-47c8-8654-09adf913734a',
+            'day': datetime.date(2023, 10, 21),
+            'name': 'dhuhr',
+            'time': datetime.time(12, 0),
+        },
+        {
+            'city_id': 'e9fa0fff-4e6a-47c8-8654-09adf913734a',
+            'day': datetime.date(2023, 10, 21),
+            'name': 'asr',
+            'time': datetime.time(14, 35),
+        },
+        {
+            'city_id': 'e9fa0fff-4e6a-47c8-8654-09adf913734a',
+            'day': datetime.date(2023, 10, 21),
+            'name': 'maghrib',
+            'time': datetime.time(16, 30),
+        },
+        {
+            'city_id': 'e9fa0fff-4e6a-47c8-8654-09adf913734a',
+            'day': datetime.date(2023, 10, 21),
+            'name': "isha'a",
+            'time': datetime.time(18, 12),
+        },
     ]
 
 
-@pytest.mark.usefixtures('_db_city', '_user', '_prayers')
+@pytest.mark.usefixtures('_db_city', '_user')
 async def test_double(pgsql):
-    nt_user_prayers_info = NtUserPrayersInfo(
+    nt_user_prayers_info = NtPgPrayersInfo(
         FkPrayersInfo({
             'city_name': 'Казань',
             'date': '21.10.2023',
@@ -101,9 +111,9 @@ async def test_double(pgsql):
             'ishaa_prayer_time': '18:12',
         }),
         pgsql,
-        FkChatId(1),
     )
     await nt_user_prayers_info.to_dict()
-    await nt_user_prayers_info.to_dict()
+    with pytest.raises(PrayersAlreadyExistsError):
+        await nt_user_prayers_info.to_dict()
 
-    assert await pgsql.fetch_val('select count(*) from prayers_at_user') == 5
+    assert await pgsql.fetch_val('select count(*) from prayers') == 6
