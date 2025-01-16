d996566669a0d79da6abe3e80f92a14b94b0b0df
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2025-01-16 21:29:59 +0300

    CdPrayersInfo object

---
+++ src/srv/prayers/cd_prayers_info.py
@@ -0,0 +1,52 @@
+# The MIT License (MIT).
+#
+# Copyright (c) 2018-2025 Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+# OR OTHER DEALINGS IN THE SOFTWARE.
+
+from typing import final, override
+
+import attrs
+import ujson
+from redis.asyncio import Redis
+
+from srv.prayers.prayer_date import PrayerDate
+from srv.prayers.prayers_info import PrayerMessageTextDict, PrayersInfo
+
+
+@final
+@attrs.define(frozen=True)
+class CdPrayersInfo(PrayersInfo):
+    """Кэширующий декоратор."""
+
+    _origin: PrayersInfo
+    _rds: Redis
+    _city_name: str
+    _date: PrayerDate
+
+    @override
+    async def to_dict(self) -> PrayerMessageTextDict:
+        """Словарь с данными для отправки пользователю."""
+        key = 'prayers:{0}:{1}'.format(self._city_name, self._date)
+        cached = await self._rds.get(key)
+        if cached:
+            return ujson.loads(cached)
+        origin = await self._origin.to_dict()
+        await self._rds.set(key, ujson.dumps(origin))
+        return origin
