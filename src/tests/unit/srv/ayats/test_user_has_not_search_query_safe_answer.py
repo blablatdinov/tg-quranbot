ec25743124f202536b49bd401403fcddcec569d4
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2025-05-28 00:54:01 +0300

    Revive fixes

---
+++ src/tests/unit/srv/ayats/test_user_has_not_search_query_safe_answer.py
@@ -0,0 +1,50 @@
+# The MIT License (MIT).
+#
+# Copyright (c) 2018-2025 Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+# OR OTHER DEALINGS IN THE SOFTWARE.
+
+from typing import final, override
+import attrs
+
+from app_types.update import Update
+from app_types.fk_update import FkUpdate
+import httpx
+from srv.ayats.user_has_not_search_query_safe_answer import UserHasNotSearchQuerySafeAnswer
+from integrations.tg.tg_answers import TgAnswer
+from integrations.tg.tg_answers.fk_answer import FkAnswer
+from exceptions.content_exceptions import UserHasNotSearchQueryError
+
+
+@final
+@attrs.define(frozen=True)
+class SeAnswer(TgAnswer):
+
+    @override
+    async def build(self, update: Update) -> list[httpx.Request]:
+        raise UserHasNotSearchQueryError
+
+
+async def test():
+    got = await UserHasNotSearchQuerySafeAnswer(
+        SeAnswer(),
+        FkAnswer(),
+    ).build(FkUpdate.empty_ctor())
+
+    assert got[0].url == 'https://some.domain'
