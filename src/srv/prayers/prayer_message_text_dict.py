d02625ac24d2bea1f19b09395e4878a57301d1d0
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2025-10-27 21:47:53 +0300

    fmt

--- src/srv/prayers/prayer_message_text_dict.py
+++ src/srv/prayers/prayer_message_text_dict.py
@@ -20,7 +20,7 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 # OR OTHER DEALINGS IN THE SOFTWARE.
 
-from typing import Protocol, TypedDict, final
+from typing import TypedDict, final
 
 
 @final
