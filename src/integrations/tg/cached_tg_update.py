66c335dfa75c4cf2ce72db7a329182c35cd19516
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2025-11-02 22:35:37 +0300

    fmt

--- src/integrations/tg/cached_tg_update.py
+++ src/integrations/tg/cached_tg_update.py
@@ -20,7 +20,7 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 # OR OTHER DEALINGS IN THE SOFTWARE.
 
-from typing import TypedDict, final, override, Final
+from typing import Final, TypedDict, final, override
 
 from app_types.update import Update
 
