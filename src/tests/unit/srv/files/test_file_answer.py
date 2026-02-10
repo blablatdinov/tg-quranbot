f158af6b58b2ab6d39f7705bee251e7dda86a1a0
Ilaletdinov Almaz <45946541+blablatdinov@users.noreply.github.com>
2026-02-10 22:57:58 +0300

    Revive fixes (#2070)

--- src/tests/unit/srv/files/test_file_answer.py
+++ src/tests/unit/srv/files/test_file_answer.py
@@ -6,11 +6,21 @@ from integrations.tg.tg_answers.fk_answer import FkAnswer
 from srv.files.file_answer import FileAnswer
 
 
-async def test():
+async def test_debug_true():
     got = await FileAnswer(
-        FkAnswer(),
-        FkAnswer(),
+        FkAnswer('https://file.id'),
+        FkAnswer('https://file.link'),
         debug_mode=True,
     ).build(FkUpdate.empty_ctor())
 
-    assert got[0].url == 'https://some.domain'
+    assert got[0].url == 'https://file.link'
+
+
+async def test_debug_false():
+    got = await FileAnswer(
+        FkAnswer('https://file.id'),
+        FkAnswer('https://file.link'),
+        debug_mode=False,
+    ).build(FkUpdate.empty_ctor())
+
+    assert got[0].url == 'https://file.id'
