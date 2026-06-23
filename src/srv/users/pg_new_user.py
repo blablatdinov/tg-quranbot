157cfd41e6db732e33851a67e239b933905b2bd8
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2026-06-23 17:03:59 +0300

    fix some tests

--- src/srv/users/pg_new_user.py
+++ src/srv/users/pg_new_user.py
@@ -4,9 +4,10 @@
 from typing import final, override
 
 import attrs
-from asyncpg import ForeignKeyViolationError, UniqueViolationError
+from asyncpg import ForeignKeyViolationError
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.exc import IntegrityError
 
 from app_types.async_int_or_none import AsyncIntOrNone
 from app_types.fk_async_int_or_none import FkAsyncIntOrNone
@@ -65,8 +66,9 @@ class PgNewUser(NewUser):
                     {'chat_id': chat_id, 'referrer_id': await self._referrer_chat_id.to_int()},
                 )
                 await conn.commit()
-        except UniqueViolationError as err:
-            raise UserAlreadyExistsError from err
+        except IntegrityError as err:
+            if 'duplicate key value violates unique constraint' in str(err):
+                raise UserAlreadyExistsError from err
         except ForeignKeyViolationError as err:
             raise UserNotFoundError from err
         self._logger.debug('User <{0}> inserted in DB'.format(chat_id))
